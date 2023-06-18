2fbe7b1d2d901a57cda4eac6679d0746e04b0eef
Mehmet Kaptan <mehmet.kaptan@ozu.edu.tr>
2023-06-18 16:43:55 +0300

    Proje dosyası ekle.

---
+++ razorpizerria/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using razorpizerria.Models;
+
+namespace razorpizerria.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
